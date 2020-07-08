531c4017b25d2afd8cee524c0844c44abe7c52ab
Sayamindu Dasgupta <sayamindu@unc.edu>
2020-07-07 20:29:07 -0400

    Support for variable number of conditions in data filter

--- src/lib/blockly/blocks/data.js
+++ src/lib/blockly/blocks/data.js
@@ -1,9 +1,35 @@
+import Blockly from "blockly/core";
+
 import {
+  BLOCKARG_DATA_BOOLEAN,
   BLOCKARG_DATA_COLUMN,
   BLOCKARG_DATA_COMPARISON_OPERATOR,
   BLOCKARG_DATA_MATCH,
-  BLOCKDROPDOWN_COMPARISON
+  BLOCKDROPDOWN_BOOLEAN,
+  BLOCKDROPDOWN_COMPARISON,
 } from "../constants";
+
+
+const ADD_ICON =
+  "data:image/svg+xml;charset=utf-8;base64,PHN2ZyB3aWR0aD0iMWVtIiBoZWlnaHQ9IjF" +
+  "lbSIgdmlld0JveD0iMCAwIDE2IDE2IiBmaWxsPSIjRjVGNUY1IiB4bWxucz0iaHR0cDovL3d3dy" +
+  "53My5vcmcvMjAwMC9zdmciPg0KICA8cGF0aCBmaWxsLXJ1bGU9ImV2ZW5vZGQiIGQ9Ik04IDMuN" +
+  "WEuNS41IDAgMCAxIC41LjV2NGEuNS41IDAgMCAxLS41LjVINGEuNS41IDAgMCAxIDAtMWgzLjVW" +
+  "NGEuNS41IDAgMCAxIC41LS41eiIvPg0KICA8cGF0aCBmaWxsLXJ1bGU9ImV2ZW5vZGQiIGQ9Ik0" +
+  "3LjUgOGEuNS41IDAgMCAxIC41LS41aDRhLjUuNSAwIDAgMSAwIDFIOC41VjEyYS41LjUgMCAwID" +
+  "EtMSAwVjh6Ii8+DQogIDxwYXRoIGZpbGwtcnVsZT0iZXZlbm9kZCIgZD0iTTggMTVBNyA3IDAgM" +
+  "SAwIDggMWE3IDcgMCAwIDAgMCAxNHptMCAxQTggOCAwIDEgMCA4IDBhOCA4IDAgMCAwIDAgMTZ6" +
+  "Ii8+DQo8L3N2Zz4=";
+
+const REMOVE_ICON =
+  "data:image/svg+xml;charset=utf-8;base64,PHN2ZyB3aWR0aD0iMWVtIiBoZWlnaHQ9IjF" +
+  "lbSIgdmlld0JveD0iMCAwIDE2IDE2IiBmaWxsPSIjRjVGNUY1IiB4bWxucz0iaHR0cDovL3d3dy" +
+  "53My5vcmcvMjAwMC9zdmciPg0KICA8cGF0aCBmaWxsLXJ1bGU9ImV2ZW5vZGQiIGQ9Ik04IDE1Q" +
+  "TcgNyAwIDEgMCA4IDFhNyA3IDAgMCAwIDAgMTR6bTAgMUE4IDggMCAxIDAgOCAwYTggOCAwIDAg" +
+  "MCAwIDE2eiIvPg0KICA8cGF0aCBmaWxsLXJ1bGU9ImV2ZW5vZGQiIGQ9Ik0zLjUgOGEuNS41IDA" +
+  "gMCAxIC41LS41aDhhLjUuNSAwIDAgMSAwIDFINGEuNS41IDAgMCAxLS41LS41eiIvPg0KPC9zdmc+";
+
+const ICON_SIZE = 25;
 
 function DataBlocks(store) {
   var blocks = {};
@@ -81,7 +107,7 @@ function DataBlocks(store) {
           {
             type: "field_input",
             name: BLOCKARG_DATA_MATCH + 0,
-            text: "10",
+            text: "condition 1",
           },
         ],
         args1: [
@@ -93,10 +119,157 @@ function DataBlocks(store) {
         inputsInline: true,
         previousStatement: null,
         nextStatement: null,
+        mutator: "data_filter_mutator",
         style: "data_blocks",
       });
     },
   };
+
+  // Mutator code below based on
+  // https://github.com/google/blockly-samples/tree/master/plugins/block-plus-minus
+
+  const dataFilterMutator = {
+    hasAdditionalCondition: false,
+
+    mutationToDom: function () {
+      const container = Blockly.utils.xml.createElement("mutation");
+      container.setAttribute(
+        "hasAdditionalCondition",
+        this.hasAdditionalCondition
+      );
+      return container;
+    },
+
+    domToMutation: function (xmlElement) {
+      const hasAdditionalCondition =
+        xmlElement.getAttribute("hasAdditionalCondition") === "true";
+      if (hasAdditionalCondition) this.addCondition();
+    },
+
+    plus: function () {
+      this.addCondition();
+    },
+
+    minus: function () {
+      this.removeCondition();
+    },
+
+    addCondition: function () {
+      const inputRow1 = this.inputList[0];
+      inputRow1
+        .appendField(
+          new Blockly.FieldDropdown(BLOCKDROPDOWN_BOOLEAN),
+          BLOCKARG_DATA_BOOLEAN + 0
+        )
+        .appendField(
+          new Blockly.FieldDropdown(generate_columns),
+          BLOCKARG_DATA_COLUMN + 1
+        )
+        .appendField(
+          new Blockly.FieldDropdown(BLOCKDROPDOWN_COMPARISON),
+          BLOCKARG_DATA_COMPARISON_OPERATOR + 1
+        )
+        .appendField(
+          new Blockly.FieldTextInput("condition 2"),
+          BLOCKARG_DATA_MATCH + 1
+        )
+        .appendField(
+          new Blockly.FieldImage(
+            REMOVE_ICON,
+            ICON_SIZE,
+            ICON_SIZE,
+            "-",
+            onMinusClicked
+          ),
+          "REMOVE"
+        )
+        .removeField("ADD");
+      this.hasAdditionalCondition = true;
+    },
+
+    removeCondition: function () {
+      const inputRow1 = this.inputList[0];
+
+      inputRow1.removeField("REMOVE");
+      inputRow1.removeField(BLOCKARG_DATA_MATCH + 1);
+      inputRow1.removeField(BLOCKARG_DATA_COMPARISON_OPERATOR + 1);
+      inputRow1.removeField(BLOCKARG_DATA_COLUMN + 1);
+      inputRow1.removeField(BLOCKARG_DATA_BOOLEAN + 0);
+
+      inputRow1.appendField(
+        new Blockly.FieldImage(
+          ADD_ICON,
+          ICON_SIZE,
+          ICON_SIZE,
+          "+",
+          onPlusClicked
+        ),
+        "ADD"
+      );
+
+      this.hasAdditionalCondition = false;
+    },
+  };
+
+  const dataFilterHelper = function () {
+    const inputRow1 = this.inputList[0];
+    inputRow1.appendField(
+      new Blockly.FieldImage(
+        ADD_ICON,
+        ICON_SIZE,
+        ICON_SIZE,
+        "+",
+        onPlusClicked
+      ),
+      "ADD"
+    );
+  };
+
+  const onPlusClicked = function (field) {
+    mutate(field, true);
+  };
+
+  const onMinusClicked = function (field) {
+    mutate(field, false);
+  };
+
+  const mutate = function (field, add) {
+    const block = field.getSourceBlock();
+
+    Blockly.Events.setGroup(true);
+
+    const oldMutationDom = block.mutationToDom();
+    const oldMutation = oldMutationDom && Blockly.Xml.domToText(oldMutationDom);
+
+    if (add) {
+      block.plus();
+    } else {
+      block.minus();
+    }
+
+    const newMutationDom = block.mutationToDom();
+    const newMutation = newMutationDom && Blockly.Xml.domToText(newMutationDom);
+
+    if (oldMutation != newMutation) {
+      Blockly.Events.fire(
+        new Blockly.Events.BlockChange(
+          block,
+          "mutation",
+          null,
+          oldMutation,
+          newMutation
+        )
+      );
+    }
+    Blockly.Events.setGroup(false);
+  };
+
+  Blockly.Extensions.unregister("data_filter_mutator");
+  Blockly.Extensions.registerMutator(
+    "data_filter_mutator",
+    dataFilterMutator,
+    dataFilterHelper
+  );
 
   return blocks;
 }
--- src/lib/primitives/data.js
+++ src/lib/primitives/data.js
@@ -1,4 +1,5 @@
 import {
+  BLOCKARG_DATA_BOOLEAN,
   BLOCKARG_DATA_COLUMN,
   BLOCKARG_DATA_COMPARISON_OPERATOR,
   BLOCKARG_DATA_MATCH,
@@ -49,36 +50,77 @@ class DataPrimTable {
   }
 
   primDataFilter(block) {
-    const columnName = block.thread.getBlockArg(block, BLOCKARG_DATA_COLUMN);
-    const operator = block.thread.getBlockArg(
-      block,
-      BLOCKARG_DATA_COMPARISON_OPERATOR
-    );
-    const match = block.thread.getBlockArg(block, BLOCKARG_DATA_MATCH);
+    const totalConditions =
+      block.mutation.hasAdditionalCondition == "true" ? 2 : 1;
+
+    var columnNames = [];
+    var operators = [];
+    var matches = [];
+
+    for (let i = 0; i < totalConditions; i++) {
+      columnNames.push(
+        block.thread.getBlockArg(block, BLOCKARG_DATA_COLUMN + i)
+      );
+      operators.push(
+        block.thread.getBlockArg(block, BLOCKARG_DATA_COMPARISON_OPERATOR + i)
+      );
+      matches.push(block.thread.getBlockArg(block, BLOCKARG_DATA_MATCH + i));
+    }
 
     const state = this.store.getState();
-    console.log(operator, match);
+
     const newData = state.projectDataState.data.filter((row) => {
-      switch (operator) {
-        case "eq":
-          // TODO: This and "ne" will need to be more clever as 'match' will be a string
-          return row[columnName] == match;
-        case "neq":
-          return row[columnName] !== match;
-        case "gt":
-          return row[columnName] > Number(match);
-        case "lt":
-          return row[columnName] < Number(match);
-        case "gte":
-          return row[columnName] >= Number(match);
-        case "lte":
-          return row[columnName] <= Number(match);
-        default:
-          console.warn(
-            `Error: got unknown operator ${operator} for ${block.type}`
-          );
+      let finalCondition;
+      
+      for (let i = 0; i < totalConditions; i++) {
+        let columnName = columnNames[i];
+        let operator = operators[i];
+        let match = matches[i];
+        let condition = false;
+        switch (operator) {
+          case "eq":
+            // TODO: This and "ne" will need to be more clever as 'match' will be a string
+            condition = row[columnName] == match;
+            break;
+          case "neq":
+            condition = row[columnName] !== match;
+            break;
+          case "gt":
+            condition = row[columnName] > Number(match);
+            break;
+          case "lt":
+            condition = row[columnName] < Number(match);
+            break;
+          case "gte":
+            condition = row[columnName] >= Number(match);
+            break;
+          case "lte":
+            condition = row[columnName] <= Number(match);
+            break;
+          default:
+            console.warn(
+              `Error: got unknown operator ${operator} while filtering rows`
+            );
+        }
+
+        if (totalConditions == 1) {
+          return condition;
+        } else if (finalCondition === undefined) {
+          finalCondition = condition;
+        } else {
+          const boolean_op =  block.thread.getBlockArg(block, BLOCKARG_DATA_BOOLEAN + (i - 1));
+          if (boolean_op === "and") {
+            finalCondition = finalCondition && condition;
+          } else {
+            finalCondition = finalCondition || condition;
+          }
+        }
+
       }
+
+      return finalCondition;
     });
+
     this.dataStack.push(state.projectDataState.data);
     this.store.dispatch({ type: PROJECT_DATA_UPDATED, payload: newData });
     this.store.dispatch({ type: PROJECT_DATA_ROW_SELECTION_RESET });
