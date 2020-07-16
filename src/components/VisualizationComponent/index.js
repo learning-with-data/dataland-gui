89b64b777034ad0bc87745d8f1f25db6c72e07e5
Sayamindu Dasgupta <sayamindu@unc.edu>
2020-07-16 00:26:42 -0400

    Revert "Use lazy loading for the visualization component"
    This reverts commit bc2cfbbb4842d4b6fd671542f8a02065506f6112.

--- src/components/VisualizationComponent/index.js
+++ src/components/VisualizationComponent/index.js
@@ -1,17 +1,13 @@
-import React, { Component, Suspense } from "react";
+import React, { Component } from "react";
 
 import PropTypes from "prop-types";
 
+import { VegaLite } from "react-vega";
 import Card from "react-bootstrap/Card";
-import Spinner from "react-bootstrap/Spinner";
 
 import { connectToRuntime } from "../connectToRuntime";
 
 import "./style.css";
-
-const VegaLite = React.lazy(() =>
-  import("react-vega").then((module) => ({ default: module.VegaLite }))
-);
 
 class VisualizationComponent extends Component {
   constructor(props) {
@@ -25,23 +21,13 @@ class VisualizationComponent extends Component {
         {/* <Card.Header className="text-right">
       </Card.Header> */}
         <Card.Body className="visualization">
-          <Suspense
-            fallback={
-              <div className="h-100 w-100 d-flex justify-content-center align-items-center">
-                <Spinner animation="border" role="status">
-                  <span className="sr-only">Loading...</span>
-                </Spinner>
-              </div>
-            }
-          >
-            <VegaLite
-              spec={this.props.projectVisualizationSpec}
-              actions={false}
-              className="h-100 w-100"
-              onParseError={(e) => console.log(e)}
-              onNewView={(v) => (this._view = v)}
-            />
-          </Suspense>
+          <VegaLite
+            spec={this.props.projectVisualizationSpec}
+            actions={false}
+            className="h-100 w-100"
+            onParseError={(e) => console.log(e)}
+            onNewView={(v) => (this._view = v)}
+          />
         </Card.Body>
       </Card>
     );
@@ -49,7 +35,7 @@ class VisualizationComponent extends Component {
 
   async _getImageBlobFromView() {
     const viewCanvas = await this._view.toCanvas(
-      320 / this._view.width() // We want a 320px wide image
+      320 / this._view.width ()// We want a 320px wide image
     );
     return new Promise((resolve) => {
       viewCanvas.toBlob((blob) => {
