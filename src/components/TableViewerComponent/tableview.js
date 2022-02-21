a799293ac8fe0aad9643972b85746989b8b97d0c
Sayamindu Dasgupta <sayamindu@gmail.com>
2022-02-21 01:14:15 -0800

    Switch to react-base-table from react-virtualized

--- src/components/TableViewerComponent/tableview.js
+++ src/components/TableViewerComponent/tableview.js
@@ -1,68 +1,120 @@
-import React from "react";
+import React, { useEffect, useRef } from "react";
 
 import PropTypes from "prop-types";
 
-import { Column, Table } from "react-virtualized";
-import AutoSizer from "react-virtualized-auto-sizer";
+import BaseTable, { AutoResizer, Column } from "react-base-table";
+import "react-base-table/styles.css";
 
-import "react-virtualized/styles.css";
+import { OverlayTrigger, Tooltip } from "react-bootstrap";
+
+const TableCell = (props) => {
+  // FIXME: Is there a better way to get the table node?
+  const tableNode = props.container.tableNode;
+  return (
+    <OverlayTrigger
+      placement="auto"
+      container={tableNode}
+      overlay={
+        <Tooltip id={`tooltip-cell-${props.rowIndex}-${props.cellData}`}>
+          {props.cellData}
+        </Tooltip>
+      }
+    >
+      <span className={props.className}>{props.cellData}</span>
+    </OverlayTrigger>
+  );
+};
+TableCell.propTypes = {
+  className: PropTypes.string,
+  cellData: PropTypes.any,
+  container: PropTypes.object,
+  rowIndex: PropTypes.number,
+};
+
+const TableHeaderCell = (props) => {
+  // FIXME: Is there a better way to get the table node?
+  const tableNode = props.container.tableNode;
+  return (
+    <OverlayTrigger
+      placement="auto"
+      container={tableNode}
+      overlay={
+        <Tooltip id={`tooltip-cell-${props.column.title}`}>
+          {props.column.title}
+        </Tooltip>
+      }
+    >
+      <span className={props.className}>{props.column.title}</span>
+    </OverlayTrigger>
+  );
+};
+TableHeaderCell.propTypes = {
+  className: PropTypes.string,
+  column: PropTypes.object,
+  container: PropTypes.object,
+};
 
 const TableView = React.memo((props) => {
-  if (props.data && props.data.length > 0) {
-    const table = (
-      <AutoSizer defaultWidth={480} defaultHeight={360}>
-        {({ height, width }) => (
-          <Table
-            className="data-table"
-            headerClassName="data-table-header"
-            headerHeight={30}
-            height={height}
-            rowCount={props.data.length}
-            rowGetter={({ index }) => props.data[index]}
-            rowHeight={40}
-            rowStyle={({ index }) => {
-              return {
-                background: props.data[index]?.__selected ? "gray" : index%2 ? "white" : "#f2f2f2",
-                color: props.data[index]?.__selected ? "white" : "black",
-              };
-            }}
-            scrollToIndex={props.data.findIndex((row) => row.__selected)}
-            width={width}
-          >
-            <Column
-              className="data-table-cell"
-              label={"Row #"}
-              dataKey={"__visible_id"}
-              key={-1}
-              width={50}
-            />
-            {props.columns.map((column, i) => {
-              return (
-                <Column
-                  className="data-table-cell"
-                  label={column}
-                  dataKey={column}
-                  key={i}
-                  width={(width - 50)/ (props.columns.length)}
-                />
-              );
-            })}
-          </Table>
-        )}
-      </AutoSizer>
-    );
+  const tableRef = useRef(null);
 
-    return table;
-  } else {
-    return (
-      <div className="p-4 data-placeholder w-100 h-100 justify-content-between align-items-center">
-        <span className="muted">
-          No data loaded. You can import data by using the &ldquo;Import
-          data&rdquo; button above.
-        </span>
-      </div>
-    );
-  }
+  useEffect(() => {
+    if (tableRef.current) {
+      tableRef.current.scrollToRow(
+        props.data.findIndex((row) => row.__selected)
+      );
+    }
+  });
+
+  return (
+    <AutoResizer>
+      {({ width, height }) => (
+        <BaseTable
+          width={width}
+          height={height}
+          data={props.data}
+          className="data-table"
+          headerClassName="data-table-header"
+          rowKey="__id"
+          ref={tableRef}
+          components={{ TableCell, TableHeaderCell }}
+          rowClassName={({ rowData, rowIndex }) => {
+            return rowData.__selected
+              ? "row-selected"
+              : "row-unselected" +
+                  " " +
+                  (rowIndex % 2 ? "row-light" : "row-dark");
+          }}
+          emptyRenderer={
+            <div className="p-4 data-placeholder w-100 h-100 justify-content-between align-items-center">
+              <span className="muted">
+                No data loaded. You can import data by using the &ldquo;Import
+                data&rdquo; button above.
+              </span>
+            </div>
+          }
+        >
+          <Column
+            className="data-table-cell"
+            title={"Row #"}
+            dataKey={"__visible_id"}
+            key={-1}
+            width={75}
+          />
+          {props.columns.map((column, i) => {
+            return (
+              <Column
+                className="data-table-cell"
+                title={column}
+                dataKey={column}
+                key={i}
+                width={(width - 75) / props.columns.length}
+              />
+            );
+          })}
+        </BaseTable>
+      )}
+    </AutoResizer>
+  );
 });
 
 TableView.propTypes = {
